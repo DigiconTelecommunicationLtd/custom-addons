000ffcb7f8cdc2810a021458d30f5977593035d2
Md. Akhter-uz-zaman <arif.cse28@gmail.com>
2018-08-14 15:11:22 +0600

    Added Updated the opportunities position.

--- isp_crm/models/isp_crm_cron_job.py
+++ isp_crm/models/isp_crm_cron_job.py
@@ -13,6 +13,21 @@ class CronJobModel(models.Model):
         today = datetime.today()
         after_7_days =  today + timedelta(days=7)
         customers_list = self.env['res.partner'].search([('customer', '=', True), ('bill_cycle_date', '=', after_7_days)])
+
+
         for customer in customers_list:
-            print(customer.email)
+            customer_package = customer
+            # name
+            # date_order
+            # partner_id
+            # partner_invoice_id
+            # partner_shipping_id
+            # pricelist_id
+
+
+            # arif-TODO-1: create a orderline with product
+            # arif-TODO-2: create a sale.order with the product
+
+
+            pass
         return True
