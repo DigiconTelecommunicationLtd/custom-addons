5e0722c2a84e2e1c24a9ed01e19509d866153270
Akhter-uz-zaman <arif.cse28@gmail.com>
2018-10-07 16:13:50 +0600

    Added employee in manifest.

--- isp_crm_module/__manifest__.py
+++ isp_crm_module/__manifest__.py
@@ -18,7 +18,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['crm', 'sale_management', 'account_invoicing'],
+    'depends': ['crm', 'sale_management', 'account_invoicing', 'hr'],
 
     # always loaded
     'data': [
