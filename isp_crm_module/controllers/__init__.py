bf76034d2adc828d9060c105516bd1dc617e62b5
TahseenAnam <tahseen.anam@cg-bd.com>
2018-10-08 11:32:01 +0600

    updated branch

--- isp_crm_module/controllers/__init__.py
+++ isp_crm_module/controllers/__init__.py
@@ -2,3 +2,4 @@
 
 from . import controllers
 from . import new_ticket_page_controller
+from . import customer_profile_ticket_controller
---
+++ isp_crm_module/static/src/js/customer_profile_update_ticket_list_api.js
@@ -0,0 +1,34 @@
+odoo.define('isp_crm_module.template_ticket_list', ['web.ajax'], function (require) {
+    "use strict";
+
+    //Define Ajax variable
+
+    var ajax = require('web.ajax');
+
+    //Json rpc call
+
+    ajax.jsonRpc("/customer/profile/ticket/", 'call', {
+
+    }).then(function (data) {
+
+         var user = data['user'];
+         var problemName = data['problemName'];
+         var problemDescription = data['problemDescription'];
+         var problemStage = data['problemStage'];
+
+         console.log(problemName);
+         console.log(problemDescription);
+         console.log(problemStage);
+
+         document.getElementById("ticket_list_table").getElementsByTagName("tbody")[0].innerHTML = "<tr><td scope=\"row\">"+problemName[0]+"</td><td>"+problemDescription[0]+"</td><td>"+problemStage[0]+"</td></tr>";
+
+         for(var i = 1; i < problemName.length; i++){
+
+            document.getElementById("ticket_list_table").getElementsByTagName("tbody")[0].innerHTML += "<tr><td scope=\"row\">"+problemName[i]+"</td><td>"+problemDescription[i]+"</td><td>"+problemStage[i]+"</td></tr>";
+
+         }
+
+    });
+
+
+});
