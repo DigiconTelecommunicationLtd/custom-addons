91b68009a30452d71db6299ca7759752787ac19b
TahseenAnam <tahseen.anam@cg-bd.com>
2018-10-14 13:01:50 +0600

    updated customer profile

--- isp_crm_module/static/src/js/customer_profile/customer_profile_api.js
+++ isp_crm_module/static/src/js/customer_profile/customer_profile_api.js
@@ -14,12 +14,41 @@ odoo.define('isp_crm_module.customer_profile_show', ['web.ajax'], function (requ
          var user = data['user'];
          var name = data['name'];
          var isPotentialCustomer = data['is_potential_customer'];
-
-         console.log(name);
-         console.log(isPotentialCustomer);
+         var subscriberId = data['subscriber_id'];
+         var father = data['father'];
+         var mother = data['mother'];
+         var birthday = data['birthday'];
+         var gender = data['gender'];
+         var identifierName = data['identifier_name'];
+         var identifierPhone = data['identifier_phone'];
+         var identifierMobile = data['identifier_mobile'];
+         var identifierNid = data['identifier_nid'];
+         var serviceType = data['service_type'];
+         var connectionType = data['connection_type'];
+         var connectionMedia = data['connection_media'];
+         var connectionStatus = data['connection_status'];
+         var billCycleDate = data['bill_cycle_date'];
+         var totalInstallationCharge = data['total_installation_charge'];
+         var packageId = data['package_id'];
 
          document.getElementById("company_name").innerHTML = name;
          document.getElementById("is_potential_customer").innerHTML = isPotentialCustomer;
+         document.getElementById("subscriber_id").innerHTML = subscriberId;
+         document.getElementById("father").innerHTML = father;
+         document.getElementById("mother").innerHTML = mother;
+         document.getElementById("birthday").innerHTML = birthday;
+         document.getElementById("gender").innerHTML = gender;
+         document.getElementById("identifier_name").innerHTML = identifierName;
+         document.getElementById("identifier_phone").innerHTML = identifierPhone;
+         document.getElementById("identifier_mobile").innerHTML = identifierMobile;
+         document.getElementById("identifier_nid").innerHTML = identifierNid;
+         document.getElementById("service_type").innerHTML = serviceType;
+         document.getElementById("connection_type").innerHTML = connectionType;
+         document.getElementById("connection_media").innerHTML = connectionMedia;
+         document.getElementById("connection_status").innerHTML = connectionStatus;
+         document.getElementById("bill_cycle_date").innerHTML = billCycleDate;
+         document.getElementById("total_installation_charge").innerHTML = totalInstallationCharge;
+         document.getElementById("package_id").innerHTML = packageId;
 
 
     });
