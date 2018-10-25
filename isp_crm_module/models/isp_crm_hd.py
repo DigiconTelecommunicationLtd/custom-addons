a109d8a76ad5402c6bb0682ff9620fd4691c3e1b
TahseenAnam <tahseen.anam@cg-bd.com>
2018-10-25 10:59:55 +0600

    updated helpdesk module

--- isp_crm_module/models/isp_crm_hd.py
+++ isp_crm_module/models/isp_crm_hd.py
@@ -23,6 +23,15 @@ class Helpdesk(models.Model):
                            help='Stage of the ticket.')
     assigned_to = fields.Many2one('hr.employee', string='Assigned To', ondelete='set null',
                                   help='Person assigned to complete the task.')
+    team = fields.Many2one('hr.department', string='Department', store=True)
+    team_leader = fields.Many2one('hr.employee', string='Team Leader', store=True)
+
+    customer = fields.Many2one('res.partner', string="Customer", domain=[('customer', '=', True)],
+                               track_visibility='onchange')
+    customer_email = fields.Char(related='customer.email', store=True)
+    customer_mobile = fields.Char(string="Mobile", related='customer.mobile', store=True)
+    customer_company = fields.Char(string="Company", related='customer.parent_id.name', store=True)
+    customer_address = fields.Char(string="Address", track_visibility='onchange')
     complexity = fields.Many2one('isp_crm_module.helpdesk_ticket_complexity', string='Complexity', ondelete='set null',
                                   help='Complexity level of the ticket.')
     solution_ids = fields.One2many('isp_crm_module.helpdesk_tasks', 'problem', string="Solutions", copy=True,
