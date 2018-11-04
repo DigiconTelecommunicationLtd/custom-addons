2d69a5c88b90ae48bfa36fcea16d0109b46cd8bc
TahseenAnam <tahseen.anam@cg-bd.com>
2018-11-04 11:35:04 +0600

    updated helpdesk and invoice

--- isp_crm_module/models/isp_crm_hd.py
+++ isp_crm_module/models/isp_crm_hd.py
@@ -2,12 +2,21 @@
 
 from odoo import api, fields, models
 from . import isp_crm_hd_ticket_history
+import datetime
 
 AVAILABLE_PRIORITIES = [
         ('0', 'Normal'),
         ('1', 'Low'),
         ('2', 'High'),
 ]
+AVAILABLE_RATINGS = [
+    ('-2', 'Worst'),
+    ('-1', 'Bad'),
+    ('0', 'Neutral'),
+    ('1', 'Good'),
+    ('2', 'Excellent'),
+]
+
 
 class Helpdesk(models.Model):
     """
@@ -15,17 +24,18 @@ class Helpdesk(models.Model):
     """
     _name = "isp_crm_module.helpdesk"
     _description = "Helpdesk"
-    _rec_name = 'type'
+    _rec_name = 'name'
     _inherit = ['mail.thread', 'mail.activity.mixin', 'utm.mixin', 'format.address.mixin']
 
     name = fields.Char('Request Name', required=True, index=True, copy=False, default='New')
-    problem = fields.Char(string="Problem", required=True, translate=True, default="Problem")
+    problem = fields.Many2one('isp_crm_module.helpdesk_problem',string="Problem", required=True, translate=True, ondelete='set null',help='Ticket Problem.')
     type = fields.Many2one('isp_crm_module.helpdesk_type', string='Type', ondelete='set null',
                                   help='Ticket Type.')
+    description = fields.Text('Description')
     stage = fields.Many2one('isp_crm_module.helpdesk_stage', string='Stage', ondelete='set null',
                            help='Stage of the ticket.', group_expand='_default_stages')
     assigned_to = fields.Many2one('hr.employee', string='Assigned To', ondelete='set null',
-                                  help='Person assigned to complete the task.')
+                                  help='Person assigned to complete the task.',track_visibility='onchange')
     team = fields.Many2one('hr.department', string='Department', store=True)
     team_leader = fields.Many2one('hr.employee', string='Team Leader', store=True)
 
@@ -41,12 +51,17 @@ class Helpdesk(models.Model):
     solution_ids = fields.One2many('isp_crm_module.helpdesk_tasks', 'problem', string="Solutions", copy=True,
                                    auto_join=True)
     priority = fields.Selection(AVAILABLE_PRIORITIES, string="Priority")
+    customer_rating = fields.Selection(AVAILABLE_RATINGS, string="Rating")
+    customer_feedback = fields.Text('Feedback')
     color = fields.Integer()
 
     @api.model
     def create(self, vals):
         first_stage = self.env['isp_crm_module.helpdesk_stage'].search([], order="sequence asc")[0]
-        vals['stage'] = first_stage.id
+        get_datetime = datetime.datetime.today().strftime('%y%m%d')
+        if vals.get('name', 'New') == 'New':
+            vals['name'] = ("T"+str(get_datetime)+self.env['ir.sequence'].next_by_code('isp_crm_module.service_request')) or '/'
+            vals['stage'] = first_stage.id
         newrecord = super(Helpdesk, self).create(vals)
         self.env['isp_crm_module.helpdesk_ticket_history'].create(
             {
@@ -61,3 +76,25 @@ class Helpdesk(models.Model):
     def _default_stages(self, stages, domain, order):
         stage_ids = self.env['isp_crm_module.helpdesk_stage'].search([('name', '!=', 'Undefined')])
         return stage_ids
+
+    @api.onchange('assigned_to')
+    def _onchange_assigned_to(self):
+        self.team_leader = self.assigned_to and self.assigned_to.parent_id
+        self.team = self.assigned_to.department_id
+
+    def get_customer_address_str(self, customer):
+        address_str = ""
+        if len(customer) > 0:
+            address_str = ", ".join([
+                customer.street or '',
+                customer.street2 or '',
+                customer.city or '',
+                customer.state_id.name or '',
+                customer.zip or '',
+                customer.country_id.name or '',
+            ])
+        return address_str
+
+    @api.onchange('customer')
+    def _onchange_customer(self):
+        self.customer_address = self.get_customer_address_str(customer=self.customer)
--- isp_crm_module/models/isp_crm_hd_ticket_history.py
+++ isp_crm_module/models/isp_crm_hd_ticket_history.py
@@ -9,10 +9,10 @@ class HelpdeskTicketHistory(models.Model):
     _name = "isp_crm_module.helpdesk_ticket_history"
     _description = "Helpdesk Ticket History"
 
+    ticket_id = fields.Many2one('isp_crm_module.helpdesk', string='Ticket', ondelete='set null',
+                                help='Ticket of the problem to solve.')
     type = fields.Many2one('isp_crm_module.helpdesk_type', string='Type', ondelete='set null',
                            help='Ticket Type.')
     assigned_to = fields.Many2one('hr.employee', string='Team', ondelete='set null',
                                   help='Person assigned to complete the task.')
-    ticket_id = fields.Many2one('isp_crm_module.helpdesk', string='Problem', ondelete='set null',
-                                help='Problem to solve.')
     color = fields.Integer()
