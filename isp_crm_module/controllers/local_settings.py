504c3f7a1e8abbb8da7d031c2151dc97c56587e0
Akhter-uz-zaman <arif.cse28@gmail.com>
2019-01-02 12:01:21 +0600

    Changed the payment controller and added a local settings file.

---
+++ isp_crm_module/controllers/local_settings.py
@@ -0,0 +1,20 @@
+# INITIATION_URL = 'https://securepay.sslcommerz.com/gwprocess/v3/api.php'
+# STORE_ID = 'digicontelecommunicationltdlive'
+# STORE_NAME = 'DIGICONTELECOLTD'
+# STORE_PWD = '5C173EC20E6A527353'
+# DEFAULT_CURRENCY = 'BDT'
+# BASE_URL = "http://localhost:8069/"
+#
+
+INITIATION_URL = 'https://sandbox.sslcommerz.com/gwprocess/v3/api.php'
+STORE_ID = 'mime5bc41d233a312'
+STORE_NAME = 'testmime24q9'
+STORE_PWD = 'mime5bc41d233a312@ssl'
+DEFAULT_CURRENCY = 'BDT'
+BASE_URL = "http://localhost:8069/"
+
+SENDGRID_API_KEY_NAME = "digico_mime_api_key"
+SENDGRID_API_KEY_NAME = "apikey"
+SENDGRID_API_KEY_ID = "2MySE-72R4ODRg_54qtAvw"
+SENDGRID_API_KEY = "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"
+SENDGRID_API_KEY = "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"
