881a351c1d3f05abfcd90bb2762da3e641f1dad9
Tahseen Anam <tahseen.adit@gmail.com>
2019-01-06 15:54:13 +0600

    updated register payment validate form

--- isp_crm_module/models/isp_crm_register_invoice_payment.py
+++ isp_crm_module/models/isp_crm_register_invoice_payment.py
@@ -13,9 +13,41 @@ class InvoiceRegisterPayment(models.Model):
 
     cheque_no = fields.Char(string='Cheque No')
     cheque_date = fields.Date('Cheque Date', default=None)
-    # amount = fields.Char(string='Amount')
     bank_name = fields.Char(string='Bank Name')
     branch_name = fields.Char(string='Branch Name')
+    is_dishonored = fields.Boolean(string='Is Dishonored')
 
+    @api.multi
+    def action_save(self):
+        """
+        Action to perform when user clicks on the save button.
+        :return:
+        """
+        is_dishonored = self.is_dishonored
+        cheque_no = self.cheque_no
+        cheque_date = self.cheque_date
+        bank_name = self.bank_name
+        branch_name = self.branch_name
+
+        if self.is_dishonored:
+            self.update({
+
+                'state': 'draft',
+                'is_dishonored': self.is_dishonored,
+                'cheque_no': self.cheque_no,
+                'cheque_date': self.cheque_date,
+                'bank_name': self.bank_name,
+                'branch_name': self.branch_name,
+
+            })
+        else:
+            self.update({
+
+                'cheque_no': self.cheque_no,
+                'cheque_date': self.cheque_date,
+                'bank_name': self.bank_name,
+                'branch_name': self.branch_name,
+
+            })
 
 
