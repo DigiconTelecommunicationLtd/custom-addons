c494eb2952a48f17a9c93e09bcf1efaffdf32ed3
Tahseen Anam <tahseen.adit@gmail.com>
2019-01-22 12:04:02 +0600

    updated files

--- isp_crm_module/models/isp_crm_hd_td_tasks.py
+++ isp_crm_module/models/isp_crm_hd_td_tasks.py
@@ -13,7 +13,7 @@ class HelpdeskTDTasks(models.Model):
     name = fields.Text('Description')
     problem = fields.Many2one('isp_crm_module.helpdesk_td', string='Problem', ondelete='set null',
                                   help='Problem to solve.')
-    solution_id = fields.Many2one('isp_crm_module.helpdesk__td_solution', string='Solution', ondelete='set null',
+    solution_id = fields.Many2one('isp_crm_module.helpdesk_td_solution', string='Solution', ondelete='set null',
                               help='Solution of the task.')
     assigned_to = fields.Many2one('hr.employee', string='Assigned To', ondelete='set null',
                               help='Person assigned to complete the task.')
