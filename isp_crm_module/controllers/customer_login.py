693d06092b55808d52b5f8a8f954de0172de0638
TahseenAnam <tahseen.anam@cg-bd.com>
2019-04-21 17:50:04 +0600

    updated corporate login to selfcare

--- isp_crm_module/controllers/customer_login.py
+++ isp_crm_module/controllers/customer_login.py
@@ -60,6 +60,20 @@ class SelfcareController(PaymentController):
         context = {}
         success_msg = ''
         if request.httprequest.method == 'POST':
+            login = request.params['login']
+            if login == "New":
+                context['error'] = _('Invalid data provided. Please provide a valid id or email address.')
+                context['success_msg'] = _(success_msg)
+                return request.render(template, context)
+            check_users = request.env['res.partner'].sudo().search([('subscriber_id', '=', str(login))])
+            for user in check_users:
+                get_opportunities = self.env['crm.lead'].search([('partner_id', '=', user.id)])
+                if get_opportunities:
+                    for opportunity in get_opportunities:
+                        if opportunity.lead_type == "corporate":
+                            context['error'] = _("Corporate User has no access right")
+                            context['success_msg'] = _(success_msg)
+                            return request.render(template, context)
             old_uid = request.uid
             uid = request.session.authenticate(request.session.db, request.params['login'], request.params['password'])
             if uid is not False:
