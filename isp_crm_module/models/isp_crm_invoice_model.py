ff566b8ba491d07880e9ec63015c7c953e223899
TahseenAnam <tahseen.anam@cg-bd.com>
2019-06-27 17:32:42 +0600

    updated issue

--- isp_crm_module/models/isp_crm_invoice_model.py
+++ isp_crm_module/models/isp_crm_invoice_model.py
@@ -55,7 +55,7 @@ class ISPCRMInvoice(models.Model):
     @api.depends('partner_id')
     def _get_lead_type(self):
         """
-        Compute type of customer .(Corporate or Retail)
+        Compute type of customer.(Corporate or Retail)
         :return:
         """
 
