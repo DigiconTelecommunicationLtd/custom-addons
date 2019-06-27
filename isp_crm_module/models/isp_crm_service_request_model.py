34367175202e5cb780fdcc9aa858314a647d0a15
TahseenAnam <tahseen.anam@cg-bd.com>
2019-06-27 17:26:38 +0600

    updated issues

--- isp_crm_module/models/isp_crm_service_request_model.py
+++ isp_crm_module/models/isp_crm_service_request_model.py
@@ -131,7 +131,7 @@ class ServiceRequest(models.Model):
     internal_notes = fields.Char(string="Internal Notes", track_visibility='onchange')
     td_flags = fields.Selection(TD_FLAGS, string="Status", track_visibility='onchange')
     is_send_for_bill_date_confirmation = fields.Boolean("Is Sent for Bill Date Confirmation", default=False)
-    billing_start_date = fields.Date(string='Billing Start Date', track_visibility='onchange')
+    billing_start_date = fields.Date(related='opportunity_ids.billing_start_date', string='Billing Start Date', track_visibility='onchange')
 
     def _get_next_package_end_date(self, given_date):
         given_date_obj = datetime.strptime(given_date, DEFAULT_DATE_FORMAT)
@@ -312,8 +312,8 @@ class ServiceRequest(models.Model):
             cust_password = self._create_random_password(size=DEFAULT_PASSWORD_SIZE)
             encrypted = "abcd1234"
 
-            if customer.billing_start_date and customer_type != "MR":
-                current_package_start_date = customer.billing_start_date
+            if service_req.billing_start_date and customer_type != "MR":
+                current_package_start_date = service_req.billing_start_date
             else:
                 current_package_start_date      = fields.Date.today()
 
@@ -325,8 +325,8 @@ class ServiceRequest(models.Model):
                 'technical_info_subnet_mask' : self.subnet_mask,
                 'technical_info_gateway' : self.gateway,
                 'description_info' : self.description,
-                'service_activation_date' : fields.Date().today(),
-                'billing_start_date' : current_package_start_date
+                'service_activation_date' : fields.Date().today()
+                # 'billing_start_date' : current_package_start_date
             })
 
             # Create an user
