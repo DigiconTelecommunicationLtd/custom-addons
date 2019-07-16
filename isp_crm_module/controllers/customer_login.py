d89cd70e9871a5ed5b20c0cffed012dbaeb66674
Akhter-uz-zaman <arif.cse28@gmail.com>
2019-07-16 12:44:33 +0600

    updated the code

--- isp_crm_module/controllers/customer_login.py
+++ isp_crm_module/controllers/customer_login.py
@@ -345,6 +345,9 @@ class SelfcareController(PaymentController):
                 service_type = request.params["service_type"]
                 transaction_id = service_type + "_" + amount
                 base_url = request.httprequest.url_root
+                #change base url when its for http
+                if base_url == 'http://crm.mimebd.com/':
+                    base_url = 'https://crm.mimebd.com/'
                 # if int(service_type) in self.SERVICE_TYPE_ID_LIST:
                 #     invoice_number = request.params["invoice_number"]
                 #     invoice_id = request.params["invoice_id"]
