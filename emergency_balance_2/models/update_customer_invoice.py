794911d99c4d5600c939552b55bd547af09e059b
Ishtiaque <ishtiaque2asad2@gmail.com>
2019-08-26 00:03:51 +0600

    [ADD] minor modification

--- emergency_balance_2/models/update_customer_invoice.py
+++ emergency_balance_2/models/update_customer_invoice.py
@@ -3,28 +3,29 @@
 from odoo import models, fields, api
 DEFAULT_DATE_FORMAT = '%Y-%m-%d'
 from datetime import datetime, timezone, timedelta, date
-
+REQUIRE_APPROVAL = 1
+APPROVED = 2
 class UpdateCustomerInvoice(models.Model):
     _inherit = "account.invoice"
     require_approval = fields.Boolean(string='approval?',
                              track_visibility='onchange',default=False)
     approval_reason = fields.Char(string='reason')
-    approved = fields.Boolean(string='approval?',
-                                      track_visibility='onchange', default=False)
-
-    approved_by=fields.Many2one('res.users', string='Approved By', track_visibility='onchange',default=False)
-
+    status = fields.Integer(default=2)
     @api.onchange('date_due')
     def date_due_thing(self):
         for record in self:
             due_date_obj = datetime.strptime(record.date_due, DEFAULT_DATE_FORMAT)
             today_new = datetime.now() + timedelta(hours=6)
             diff =abs((due_date_obj - today_new).days)
+            diff = diff + 1
             if diff > 10:
-                record.require_approval = True
+               record.status = REQUIRE_APPROVAL
             else:
-                record.require_approval = False
-            print(str(record.require_approval))
+                record.status = APPROVED
+
+            print(today_new)
+            print (due_date_obj)
+            print(str(record.status))
             # modified_date_obj = due_date_obj + timedelta(days=1, hours=6)
             # record.new_next_start_date = modified_date_obj.strftime(DEFAULT_DATE_FORMAT)
             # print(record.date_due)
@@ -33,5 +34,5 @@ class UpdateCustomerInvoice(models.Model):
     @api.one
     def review_for_defer(self):
         for record in self:
-            record.approved = True
+            record.status = APPROVED
 
