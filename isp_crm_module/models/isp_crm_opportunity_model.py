1f8a87d024957f35158af776bb15ca475c56896a
RiponBasak <ripon.kumar@cg-bd.com>
2019-08-27 09:25:55 +0600

    [MOD][isp_crm_models, isp_invoice_module] invoice connectivity address

--- isp_crm_module/models/isp_crm_opportunity_model.py
+++ isp_crm_module/models/isp_crm_opportunity_model.py
@@ -3,10 +3,12 @@
 
 
 import re
-from odoo import api, fields, models, _
+from odoo import api, fields, models, _ ,tools
 from odoo.exceptions import Warning, UserError
 from . import isp_crm_service_request_model
 from datetime import datetime, timezone, timedelta, date
+from odoo.tools.translate import _
+from odoo.tools import email_re, email_split
 
 DEFAULT_PROBLEM = "There are some Problem"
 INVOICE_PAID_STATUS = 'paid'
@@ -46,6 +48,43 @@ class Opportunity(models.Model):
     proposed_activation_date = fields.Date(string='Proposed Service Activation Date', track_visibility='onchange')
     billing_start_date = fields.Date(string='Billing Start Date', track_visibility='onchange')
     connectivityAddress = fields.Char('Connectivity Address',size=100)
+
+
+
+    @api.multi
+    def _create_lead_partner_data(self, name, is_company, parent_id=False):
+        """ extract data from lead to create a partner
+            :param name : furtur name of the partner
+            :param is_company : True if the partner is a company
+            :param parent_id : id of the parent partner (False if no parent)
+            :returns res.partner record
+        """
+        email_split = tools.email_split(self.email_from)
+        print("######################")
+        return {
+            'name': name,
+            'user_id': self.env.context.get('default_user_id') or self.user_id.id,
+            'comment': self.description,
+            'team_id': self.team_id.id,
+            'parent_id': parent_id,
+            'phone': self.phone,
+            'mobile': self.mobile,
+            'email': email_split[0] if email_split else False,
+            'title': self.title.id,
+            'function': self.function,
+            'street': self.street,
+            'street2': self.street2,
+            'zip': self.zip,
+            'city': self.city,
+            'country_id': self.country_id.id,
+            'state_id': self.state_id.id,
+            'website': self.website,
+            'is_company': is_company,
+            'type': 'contact',
+            'connectivity_address': self.connectivityAddress
+        }
+
+
 
 
     def _get_color_on_service_request_status(self):
--- isp_crm_old/models/isp_crm_states.py
+++ isp_crm_old/models/isp_crm_states.py
@@ -10,3 +10,37 @@ class IspCRMStates(models.Model):
     @api.multi
     def _is_state_done(self):
         return True
+
+    @api.multi
+    def _create_lead_partner_data(self, name, is_company, parent_id=False):
+        """ extract data from lead to create a partner
+            :param name : furtur name of the partner
+            :param is_company : True if the partner is a company
+            :param parent_id : id of the parent partner (False if no parent)
+            :returns res.partner record
+        """
+        email_split = tools.email_split(self.email_from)
+        console.log("######################")
+        return {
+            'name': name,
+            'user_id': self.env.context.get('default_user_id') or self.user_id.id,
+            'comment': self.description,
+            'team_id': self.team_id.id,
+            'parent_id': parent_id,
+            'phone': self.phone,
+            'mobile': self.mobile,
+            'email': email_split[0] if email_split else False,
+            'title': self.title.id,
+            'function': self.function,
+            'street': self.street,
+            'street2': self.street2,
+            'zip': self.zip,
+            'city': self.city,
+            'country_id': self.country_id.id,
+            'state_id': self.state_id.id,
+            'website': self.website,
+            'is_company': is_company,
+            'type': 'contact',
+            'connectivityAddress': self.connectivityAddress
+        }
+
